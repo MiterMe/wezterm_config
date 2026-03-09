60bc8298c477c448804a582b5d14177431545567
Miter <miter.me@outlook.com>
2026-03-09 18:38:34 +0800

    change theme

--- miter/themes.lua
+++ miter/themes.lua
@@ -1,7 +1,8 @@
 local M = {}
 
 function M.load(config)
-	config.color_scheme = "Tokyo Night Storm (Gogh)"
+	-- config.color_scheme = "Tokyo Night Storm (Gogh)"
+	config.color_scheme = "Everforest Dark Soft (Gogh)"
 end
 
 return M
