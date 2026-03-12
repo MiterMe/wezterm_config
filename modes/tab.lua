70a382b92d77ede41c22218eebcb83eca835c06e
Miter <miter.me@outlook.com>
2026-03-12 14:55:38 +0800

    修改Tab Title

--- modes/tab.lua
+++ modes/tab.lua
@@ -3,4 +3,20 @@ local act = wezterm.action
 
 return {
 	{ key = "n", action = act.SpawnTab("CurrentPaneDomain") },
+	{
+		key = "r",
+		action = act.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for this Tab: " },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				local tab = window:active_tab()
+				if line and #line > 0 then
+					tab:set_title(line)
+				end
+			end),
+		}),
+	},
 }
