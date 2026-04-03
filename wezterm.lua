989c10e7d802720194ad4243da67a3b950c50dd1
Miter <miter.me@outlook.com>
2026-04-03 17:51:00 +0800

    del domains

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- local domains = require("miter.domains")
 local themes = require("miter.themes")
 local ui = require("miter.ui")
 local fonts = require("miter.fonts")
 -- local events = require("miter.events")
 local keys = require("miter.keys")
 
--- domains.load(config)
 themes.load(config)
 ui.load(config)
 fonts.load(config)
