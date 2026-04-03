e49d04af04eed9af07939fb1f7b9a004a0dc63ab
Miter <miter.me@outlook.com>
2026-04-03 21:49:59 +0800

    add themes

--- miter/themes.lua
+++ miter/themes.lua
@@ -1,7 +1,10 @@
 local M = {}
 
 function M.load(config)
-	config.color_scheme = "Everforest Dark Soft (Gogh)"
+	-- config.color_scheme = "Everforest Dark (Gogh)"
+	-- config.color_scheme = "Everforest Dark Hard (Gogh)"
+	config.color_scheme = "Everforest Dark Medium (Gogh)"
+	-- config.color_scheme = "Everforest Dark Soft (Gogh)"
 end
 
 return M
