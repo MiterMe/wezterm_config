ed0664fd9bdc46f34248ec3ddcb56f9122e5db43
Miter <miter.me@outlook.com>
2026-04-03 19:00:33 +0800

    webgpu

--- miter/ui.lua
+++ miter/ui.lua
@@ -1,14 +1,12 @@
 local M = {}
 
 function M.load(config)
+	config.enable_wayland = true
+	config.prefer_egl = true
 	config.front_end = "WebGpu"
 	config.webgpu_power_preference = "HighPerformance"
-
 	config.enable_tab_bar = false
 	config.hide_tab_bar_if_only_one_tab = true
-
-	config.max_fps = 60
-	config.animation_fps = 1
 end
 
 return M
