08139e60f507cecb6a46ffbbf83c33209cd9b2ab
Miter <miter.me@outlook.com>
2026-04-03 13:37:55 +0800

    restore to start gui

--- wezterm.lua
+++ wezterm.lua
@@ -1,18 +1,18 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-local domains = require("miter.domains")
+-- local domains = require("miter.domains")
 local themes = require("miter.themes")
 local ui = require("miter.ui")
 local fonts = require("miter.fonts")
-local events = require("miter.events")
-local keys = require("miter.keys")
+-- local events = require("miter.events")
+-- local keys = require("miter.keys")
 
-domains.load(config)
+-- domains.load(config)
 themes.load(config)
 ui.load(config)
 fonts.load(config)
-events.load()
-keys.load(config)
+-- events.load()
+-- keys.load(config)
 
 return config
